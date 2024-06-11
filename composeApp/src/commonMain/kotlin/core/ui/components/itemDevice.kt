6fda270ae6994cff7614267a3c22958da9313683
Vojin Bjelica <vojin.bjelica@vegait.rs>
2024-06-11 13:04:13 +0200

    Fix device card padding

--- composeApp/src/commonMain/kotlin/core/ui/components/itemDevice.kt
+++ composeApp/src/commonMain/kotlin/core/ui/components/itemDevice.kt
@@ -43,7 +43,7 @@ fun itemDevice(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 8.dp),
+            .padding(bottom = 8.dp, start = 8.dp, end = 8.dp),
         shape = RoundedCornerShape(25.dp),
     ) {
         Surface(
